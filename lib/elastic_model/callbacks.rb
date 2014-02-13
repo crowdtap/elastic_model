fd516cbd3f255dfe510a2b20f796e0d4f2ed898d
Quentin Decock <quentin.decock@gmail.com>
2014-02-13 17:29:59 -0500

    wip

--- lib/elastic_model/callbacks.rb
+++ lib/elastic_model/callbacks.rb
@@ -4,15 +4,5 @@ module ElasticModel::Callbacks
   included do
     after_save    :save_to_es
     after_destroy :delete_from_es
-
-    def save_to_es
-      if self.changed?
-        $es.index :index => self.class.es_index_name, :type => self.class.es_type, :id => self.id, :body => self.as_json
-      end
-    end
-
-    def delete_from_es
-      $es.delete :index => self.class.es_index_name, :type => self.class.es_type, :id => self.id
-    end
   end
 end
